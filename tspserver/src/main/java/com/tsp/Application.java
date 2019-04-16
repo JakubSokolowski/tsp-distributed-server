1a47bac226d998d7c61d11c90c3f54d77e1ebee6
AdamKoliweszka <adamkoliweszka@gmail.com>
2019-04-16 13:09:34 +0200

    Delete comment

--- tspserver/src/main/java/com/tsp/Application.java
+++ tspserver/src/main/java/com/tsp/Application.java
@@ -1,15 +1,6 @@
 package com.tsp;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author user
- */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- tspserver/src/main/java/com/tsp/bean/User.java
+++ tspserver/src/main/java/com/tsp/bean/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.tsp.bean;
 
 import java.io.Serializable;
@@ -13,10 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- *
- * @author Adam
- */
+
 @Entity
 @Table(name = "users")
 public class User implements Serializable{
