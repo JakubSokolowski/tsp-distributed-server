449c1b7058b670a28bfa23ea0aba85c7b13fe489
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-06-02 11:27:17 +0200

    Move spring application startup to front.

--- tspserver/src/main/java/com/tsp/Application.java
+++ tspserver/src/main/java/com/tsp/Application.java
@@ -1,39 +1,16 @@
 package com.tsp;
 
 
-import com.tsp.cluster.job.BruteForceJobContext;
+import com.tsp.cluster.WorkerManager;
 import com.tsp.cluster.runner.BruteForceJobRunner;
-import com.tsp.cluster.instance.HardcodedInstanceProvider;
-import com.tsp.cluster.instance.ProblemInstance;
-import com.tsp.cluster.task.provider.BruteForceTaskProvider;
-import com.tsp.service.SolvingThread;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    SolvingThread s;
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-
-        /*
-        ProblemInstance problemInstance = new HardcodedInstanceProvider().getProblemInstance();
-        BruteForceTaskProvider bftp = new BruteForceTaskProvider(problemInstance.getGraph());
-        BruteForceJobContext context = new BruteForceJobContext(bftp);
-        BruteForceJobRunner rn = new BruteForceJobRunner(context);
-        new Thread(rn).start();
-        System.out.println("Starting Server");
-        */
-
-    }
-    @EventListener(ApplicationReadyEvent.class)
-    public void doSomethingAfterStartup() {
-        s.start();
+        BruteForceJobRunner runner = new BruteForceJobRunner(new WorkerManager());
+        new Thread(runner).start();
     }
 }
