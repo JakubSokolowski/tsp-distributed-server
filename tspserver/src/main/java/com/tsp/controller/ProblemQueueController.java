db013e0bfce9a0275ddfa386e8388e68c6e1b123
AdamKoliweszka <adamkoliweszka@gmail.com>
2019-06-05 20:51:11 +0200

    add problem queue controller

---
+++ tspserver/src/main/java/com/tsp/controller/ProblemQueueController.java
@@ -0,0 +1,46 @@
+package com.tsp.controller;
+
+import com.tsp.cluster.instance.ProblemInstance;
+import com.tsp.service.ProblemInstanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/Queue")
+public class ProblemQueueController {
+
+    @RequestMapping(value = "/Up",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity upProblem(@RequestBody Integer id){
+        //tutaj przesunięcie do przodu w kolejce
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @RequestMapping(value = "/Down",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity downProblem(@RequestBody Integer id){
+        //tutaj przesunięcie do tyłu w kolejce
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/Stop",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity stopProblem(@RequestBody Integer id){
+        //tutaj start rozwiązywania problemu
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    @RequestMapping(value = "/Start",method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity startProblem(@RequestBody Integer id){
+        //tutaj stop rozwiązywania problemu
+        return new ResponseEntity(HttpStatus.OK);
+    }
+    
+
+    
+
+
+}
